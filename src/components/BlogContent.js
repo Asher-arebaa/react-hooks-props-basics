d18d5909ec5cdb6d664283a3d60e11aa140392b3
Asher <Asher.areba@gmail.com>
2023-01-12 20:42:11 +0300

    Completed assignemnt

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+function BlogPost() {
+  return (
+    <div>
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    </div>
+  );
 }
 
 export default BlogContent;
